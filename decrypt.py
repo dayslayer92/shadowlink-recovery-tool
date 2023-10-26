9f223a788d237d98c4a828a917e4c95ab77102d6
netherops-dev <netherops-dev@shadowlink.local>
2023-10-26 16:22:44 +0200

    Implement Caesar cipher shift decryption algorithm

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import os
+import binascii
+import string
+import base64
 
 def load_env(path):
     env = {}
@@ -18,6 +21,27 @@ def load_env(path):
             sys.exit(1)
     return env
 
+def caesar_decrypt(text: str, shift: int, alphabet: str) -> str:
+    if not text:
+        return text
+    alpha_len = len(alphabet)
+    alpha_index = {ch: i for i, ch in enumerate(alphabet)}
+
+    out_chars = []
+    for ch in text:
+        lower_ch = ch.lower()
+        if lower_ch in alpha_index:
+            idx = alpha_index[lower_ch]
+            new_idx = (idx - shift) % alpha_len
+            new_ch = alphabet[new_idx]
+            if new_ch.isalpha() and ch.isalpha():
+                out_chars.append(new_ch.upper() if ch.isupper() else new_ch)
+            else:
+                out_chars.append(new_ch)
+        else:
+            out_chars.append(ch)
+    return "".join(out_chars)
+
 def decode_file(path):
     with open(path, "rb") as f:
         data = f.read()
@@ -30,8 +54,14 @@ def decode_file(path):
         sys.stderr.write('Error: "SHADOW_KEY" environment variable not set in .env\n')
         sys.exit(1)
     
-    # TODO: Implement decryption logic using key
-    return data.decode("utf-8", errors="replace")
+    alphabet = string.ascii_lowercase
+    shift = (sum(ord(c) for c in key) % 64) % len(alphabet)
+    
+    encrypted_text = data.decode("utf-8", errors="replace")
+    decrypted_text = caesar_decrypt(encrypted_text, shift, alphabet)
+    
+    # TODO: Add Base64 decoding
+    return decrypted_text
 
 def main():
     if len(sys.argv) < 2:
