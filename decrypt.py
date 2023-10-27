e171a9c2675f506edf3b16bb7e2c18d61feaafae
netherops-dev <netherops-dev@shadowlink.local>
2023-10-27 11:08:19 +0100

    Add Base64 decoding with padding support

--- decrypt.py
+++ decrypt.py
@@ -42,38 +42,50 @@ def caesar_decrypt(text: str, shift: int, alphabet: str) -> str:
             out_chars.append(ch)
     return "".join(out_chars)
 
+def safe_b64_decode(data: bytes, pad=True):
+    data = b"".join(data.split())
+    if pad:
+        data += b"=" * ((4 - len(data) % 4) % 4)
+    try:
+        return base64.b64decode(data)
+    except binascii.Error:
+        return base64.urlsafe_b64decode(data)
+
 def decode_file(path):
+    mode = "whole"
+
     with open(path, "rb") as f:
         data = f.read()
-    
+
     env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     env = load_env(env_path)
     key = env.get("SHADOW_KEY", "")
-    
+
     if not key:
         sys.stderr.write('Error: "SHADOW_KEY" environment variable not set in .env\n')
         sys.exit(1)
-    
+
     alphabet = string.ascii_lowercase
     shift = (sum(ord(c) for c in key) % 64) % len(alphabet)
-    
+
     encrypted_text = data.decode("utf-8", errors="replace")
     decrypted_text = caesar_decrypt(encrypted_text, shift, alphabet)
-    
-    # TODO: Add Base64 decoding
-    return decrypted_text
+
+    decoded = safe_b64_decode(decrypted_text.encode("ascii", errors="ignore"), True)
+
+    return decoded.decode("utf-8", errors="replace")
 
 def main():
     if len(sys.argv) < 2:
         sys.stderr.write('Error: No input file specified.\n')
         sys.stderr.write('Usage: python decrypt.py <input_file.shadow>\n')
         sys.exit(1)
-    
+
     input_file = sys.argv[1]
     if not os.path.exists(input_file):
         sys.stderr.write(f'Error: "{input_file}" file not found.\n')
         sys.exit(1)
-    
+
     print(decode_file(input_file))
 
 if __name__ == "__main__":
